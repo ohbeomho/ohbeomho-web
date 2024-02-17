34e4576eed94347a75afc3222df49bf41669e48a
OhBeomho <tyler0322@naver.com>
2024-02-17 11:05:45 +0900

    better mobile support marked lowest, highest rank in graph

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+const isMobile = (function (a) {
+  if (
+    /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+      a
+    ) ||
+    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+      a.substr(0, 4)
+    )
+  )
+    return true
+  else return false
+})(navigator.userAgent || navigator.vendor || window.opera)
+
 const ageElement = document.querySelector('.age')
-ageElement.innerHTML += ' ' + (new Date().getFullYear() - 2009)
+ageElement.innerHTML += ' <span>' + (new Date().getFullYear() - 2009) + '</span>'
 
 const container = document.querySelector('.container')
 const ohbeomhoText = document.querySelector('h1')
@@ -10,15 +23,15 @@ const animateTexts = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]()12
 const animateElements = [
   {
     title: 'INFO',
-    elements: '#info p'
+    elements: '#info p span'
   },
   {
     title: 'OSU!',
-    elements: '.rank div span, .pp, .rank-history div'
+    elements: '.rank div span, .pp, .rank-history span'
   },
   {
     title: 'CODING',
-    elements: '#coding p'
+    elements: '#coding p span'
   }
 ]
 
@@ -102,56 +115,61 @@ document.querySelectorAll('.up').forEach(element => {
 })
 
 const cursor = document.querySelector('.cursor')
-const mousePos = { x: 0, y: 0 }
-const prevMousePos = { x: 0, y: 0 }
-let size = 50,
-  currSize = 50
-const clickElements = ['button', 'title-text', 'avatar']
 
-window.addEventListener('mousemove', e => {
-  const { pageX: x, pageY: y } = e
+if (!isMobile) {
+  const mousePos = { x: 0, y: 0 }
+  const prevMousePos = { x: 0, y: 0 }
+  let size = 50,
+    currSize = 50
+  const clickElements = ['button', 'title-text', 'avatar']
 
-  mousePos.x = x
-  mousePos.y = y
+  window.addEventListener('mousemove', e => {
+    const { pageX: x, pageY: y } = e
 
-  if (e.target.classList && clickElements.some(element => e.target.classList.contains(element))) {
-    size = 150
-  } else {
-    size = 50
+    mousePos.x = x
+    mousePos.y = y
+
+    if (e.target.classList && clickElements.some(element => e.target.classList.contains(element))) {
+      size = 150
+    } else {
+      size = 50
+    }
+  })
+
+  function parallax(element, xdiff, ydiff) {
+    element.style.transform = `translateX(${xdiff / 150}px) translateY(${ydiff / 150}px)`
+    element.childNodes.forEach(child => child instanceof Element && parallax(child, xdiff, ydiff))
   }
-})
 
-function parallax(element, xdiff, ydiff) {
-  element.style.transform = `translateX(${xdiff / 150}px) translateY(${ydiff / 150}px)`
-  element.childNodes.forEach(child => child instanceof Element && parallax(child, xdiff, ydiff))
-}
+  function animate() {
+    const distX = mousePos.x - prevMousePos.x
+    const distY = mousePos.y - prevMousePos.y
+    const dist = Math.sqrt(distX * distX + distY * distY)
+    const delta = Math.min(Math.abs(dist), 120)
+    const angle = Math.atan2(distY, distX)
 
-function animate() {
-  const distX = mousePos.x - prevMousePos.x
-  const distY = mousePos.y - prevMousePos.y
-  const dist = Math.sqrt(distX * distX + distY * distY)
-  const delta = Math.min(Math.abs(dist), 120)
-  const angle = Math.atan2(distY, distX)
+    prevMousePos.x = mousePos.x
+    prevMousePos.y = mousePos.y
 
-  prevMousePos.x = mousePos.x
-  prevMousePos.y = mousePos.y
+    currSize += (size - currSize) / 6
 
-  currSize += (size - currSize) / 6
+    cursor.style.left = `${mousePos.x - currSize / 2}px`
+    cursor.style.top = `${mousePos.y - currSize / 2}px`
+    cursor.style.width = cursor.style.height = `${currSize}px`
+    cursor.style.transform = `rotate(${angle}rad) scaleX(${1 + delta / 100}) scaleY(${1 - delta / 240})`
 
-  cursor.style.left = `${mousePos.x - currSize / 2}px`
-  cursor.style.top = `${mousePos.y - currSize / 2}px`
-  cursor.style.width = cursor.style.height = `${currSize}px`
-  cursor.style.transform = `rotate(${angle}rad) scaleX(${1 + delta / 100}) scaleY(${1 - delta / 240})`
+    const xdiff = mousePos.x - window.innerWidth / 2
+    const ydiff = mousePos.y - window.innerHeight / 2
+    parallax(container, xdiff, ydiff)
+    ohbeomhoText.style.transform = `translateX(calc(${(xdiff / 150) * 2}px - 50%)) translateY(${(ydiff / 150) * 2}px)`
 
-  const xdiff = mousePos.x - window.innerWidth / 2
-  const ydiff = mousePos.y - window.innerHeight / 2
-  parallax(container, xdiff, ydiff)
-  ohbeomhoText.style.transform = `translateX(calc(${(xdiff / 150) * 2}px - 50%)) translateY(${(ydiff / 150) * 2}px)`
+    requestAnimationFrame(animate)
+  }
 
   requestAnimationFrame(animate)
+} else {
+  cursor.style.display = 'none'
 }
-
-requestAnimationFrame(animate)
 
 const profileElement = document.getElementById('profile')
 
@@ -212,17 +230,17 @@ function getProfile() {
         <span class='username'>${userData.username}</span>
         <div class='rank'>
           <div>
-            Global Ranking<br />
+            <b>Global Ranking</b><br />
             <span>#${userData.statistics.global_rank}</span>
           </div>
           <div>
-            Country Ranking<br />
+            <b>Country Ranking</b><br />
             <span>#${userData.statistics.country_rank}</span>
           </div>
         </div>
         <span class='pp'>${userData.statistics.pp}pp</span>
         <div>
-          <div><div>Rank History</div></div>
+          <div><b>Rank History</b></div>
           <div>
             <div>
               <canvas class='graph' height='150'></canvas>
@@ -230,16 +248,15 @@ function getProfile() {
           </div>
           <div style='display: flex; justify-content: space-between; align-items: center' class='rank-history'>
             <div>
-              Lowest: #${lowest}
+              <b style='color: red'>Lowest</b> <span>#${lowest}</span>
             </div>
             <div>
-              Highest: #${highest}
+              <b style='color: rgb(0, 255, 0); text-shadow: 0 0 white'>Highest</b> <span>#${highest}</span>
             </div>
           </div>
         </div>
       `
 
-      // Graph animation maybe?
       const graph = profileElement.querySelector('.graph')
       const context = graph.getContext('2d')
 
@@ -247,13 +264,13 @@ function getProfile() {
       graph.height = graph.offsetHeight
 
       context.strokeStyle = 'white'
-      context.lineWidth = 3
+      context.lineWidth = 2
 
       const previousPos = { x: 0, y: 0 }
 
       for (let i = 0; i < rankHistory.length; i++) {
-        const x = i * ((graph.offsetWidth - 40) / 90) + 20
-        const y = 5 + ((rankHistory[i] - highest) / (lowest - highest)) * 140
+        const x = i * ((graph.offsetWidth - 20) / 90) + 10
+        const y = 10 + ((rankHistory[i] - highest) / (lowest - highest)) * (graph.height - 20)
 
         if (i > 0) {
           context.beginPath()
@@ -264,6 +281,33 @@ function getProfile() {
 
         previousPos.x = x
         previousPos.y = y
+      }
+
+      let markedLowest = false,
+        markedHighest = false
+
+      for (let i = 0; i < rankHistory.length; i++) {
+        if (markedLowest && markedHighest) break
+
+        if (rankHistory[i] === lowest || rankHistory[i] === highest) {
+          const x = i * ((graph.offsetWidth - 20) / 90) + 10
+          const y = 10 + ((rankHistory[i] - highest) / (lowest - highest)) * (graph.height - 20)
+
+          if (rankHistory[i] === lowest) {
+            context.fillStyle = 'red'
+            markedLowest = true
+          } else if (rankHistory[i] === highest) {
+            context.fillStyle = 'rgb(0, 255, 0)'
+            context.beginPath()
+            context.arc(x, y, 3, 0, 2 * Math.PI)
+            context.stroke()
+            markedHighest = true
+          }
+
+          context.beginPath()
+          context.arc(x, y, 3, 0, 2 * Math.PI)
+          context.fill()
+        }
       }
 
       const avatar = profileElement.querySelector('.avatar')
